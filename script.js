edb7f868e7b2a3fd1ae85fee7fe377ecc3e986fa
Saurabh Purohit <grv4697@gmail.com>
2026-06-26 10:15:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const header = document.querySelector("[data-header]");
+
+const syncHeader = () => {
+  header.classList.toggle("is-scrolled", window.scrollY > 12);
+};
+
+syncHeader();
+window.addEventListener("scroll", syncHeader, { passive: true });
